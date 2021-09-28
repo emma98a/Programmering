1f98e914a2ed1bfa6f65c9ad916d9f0a2a5d1201
Emma Andersen <emma.soleil98@gmail.com>
2021-09-28 12:47:38 +0200

    Tilføjer uge 39 javascript fil

---
+++ script39.js
@@ -0,0 +1,5 @@
+console.log('script.js loaded');
+console.log(document.getElementById('header').innerHTML);
+
+// You can write JavaScript below this comment and it will load when you open index.html
+
